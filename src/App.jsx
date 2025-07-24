c78811257aa04f8d4ae4b939ed50815807ea4d83
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-07-24 23:42:13 +0600

    router basic

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './NotFound'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
